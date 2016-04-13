1e9d426d43adc1ef8549e198139a1fb9abd46686
Juan Cortes <juancoob@gmail.com>
2016-04-12 20:08:59 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('body').mousemove(function(event) {
+        cx = Math.ceil($('body').width() / 2.0);
+        cy = Math.ceil($('body').height() / 2.0);
+        dx = event.pageX - cx;
+        dy = event.pageY - cy;
+         // console.log('center location'+cx+','+cy);
+         // console.log('distances'+dx+','+dy);
+        tiltx = - (dy / cy);
+        tilty =  (dx / cx);
+        radius = Math.sqrt(Math.pow(tiltx,2) + Math.pow(tilty,2));
+        degree = (radius * 20);
+
+        $('#cube').css('-webkit-transform','rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
+    });
+});
